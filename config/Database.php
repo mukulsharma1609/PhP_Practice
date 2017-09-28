199b22a040ad9ca3eef79b015cd34a3b49899b17
Mukul Sharma <mukul.sharma@accoliteindia.com>
2017-09-28 11:45:19 +0530

    db connection

---
+++ config/Database.php
@@ -0,0 +1,21 @@
+<?php
+
+class Database {
+
+	public $con;
+	public function __construct(){
+		$this->con = mysqli_connect("localhost","root","","php_practice");
+		if($this->con){
+			echo "Connected";
+		}
+		else {
+			echo "Error in Connection";
+		}
+
+	}
+}
+
+
+$obj = new Database;
+
+?>
---
+++ sign_up.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		
+	</title>
+</head>
+<body>
+
+	<h3>Sign Up</h3>
+	<label>Name : </label>
+	<input type="text" name="user_name" required></br>
+	<label>Email : </label>
+	<input type="email" name="user_email" required></br>
+	<label>Password : </label>
+	<input type="password" name="user_password"></br>
+	<input type="submit" name="register" value="Register">
+
+</body>
+</html>
